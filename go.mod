d6264b87d7dc2caf25a30db273d0029608df5caf
Miguel Borges <up202004481@edu.fe.up.pt>
2023-04-22 20:57:26 +0100

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/miguelrcborges/html2js
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"os"
+	"bufio"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	f, err := os.Create("compiled.js")
+	check(err)
+
+	defer f.Close()
+
+	w := bufio.NewWriter(f);
+	
+	for _, file := range os.Args[1:] {
+		compileComponent(w, file);
+	}
+	err = w.Flush()
+	check(err)
+}
+
+func compileComponent(w *bufio.Writer, filePath string) {
+	f, err := os.Open(filePath)
+	check(err)
+	defer f.Close()
+
+	filePathSplitted := strings.Split(filePath, "/")
+	fileName := filePathSplitted[len(filePathSplitted) - 1]
+	componentName := strings.Split(fileName, ".")[0]
+	w.Write([]byte("const "))
+	w.WriteString(componentName)
+	w.Write([]byte("=()=>{"))
+
+	_ = bufio.NewReader(f)
+
+	w.Write([]byte("};"))
+}
