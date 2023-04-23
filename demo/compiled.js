94870de0383b382e18e8aaeb22ce31d56f2396c0
Miguel Borges <up202004481@edu.fe.up.pt>
2023-04-23 02:24:09 +0100

    Able to compile the whole element tree
    still needs to be added the ability to add classes and ids, but text content works fine

--- demo/compiled.js
+++ demo/compiled.js
@@ -1,1 +1,1 @@
-const componentOne=()=>{};const componentTwo=()=>{};
+const componentOne=()=>{let e=document.createElement('div');e.setAtrribute('id','componentOne');let e0=document.createElement('div');let e1=document.createElement('h1');e1.textContent='Some text';e0.appendChild(e1);e.appendChild(e0);return e;};const componentTwo=()=>{let e=document.createElement('div');e.setAtrribute('id','componentTwo');let e2=document.createElement('div');let e3=document.createElement('button');e3.textContent='Click me';e2.appendChild(e3);e.appendChild(e2);return e;};
--- main.go
+++ main.go
@@ -2,9 +2,31 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"fmt"
 	"os"
 	"strings"
 )
+
+var elementCount int
+
+func getHTMLElements() [11]string {
+	return [11]string {
+		"div", "a", "button", "form",
+		"h1", "h2", "h3", "h4", "h5", "h6",
+		"p", 
+	}
+}
+
+func isAnHTMLElement(e string) bool {
+	elements := getHTMLElements();
+	for _, v := range elements {
+		if e == v {
+			return true
+		}
+	}
+	return false
+}
 
 func check(e error) {
 	if e != nil {
@@ -37,9 +59,64 @@ func compileComponent(w *bufio.Writer, filePath string) {
 	componentName := strings.Split(fileName, ".")[0]
 	w.Write([]byte("const "))
 	w.WriteString(componentName)
-	w.Write([]byte("=()=>{"))
+	w.Write([]byte("=()=>{let e=document.createElement('div');e.setAtrribute('id','"))
+	w.WriteString(componentName)
+	w.Write([]byte("');"))
 
-	_ = bufio.NewReader(f)
+	r := bufio.NewReader(f)
+	
+	for {
+		_, err := r.ReadBytes('<')
+		if err != nil {
+			break;
+		} 	
 
-	w.Write([]byte("};"))
+		num := proccessElement(w, r)
+		w.WriteString(fmt.Sprintf("e.appendChild(e%d);", num))
+	}
+
+	w.Write([]byte("return e;};"))
+}
+
+
+func proccessElement(w *bufio.Writer, r *bufio.Reader) int {
+	elemNumber := elementCount
+	elementCount++
+	wholeTag, _ := r.ReadString('>')
+	stuff := strings.Split(wholeTag, " ")
+
+	// remove the '>' lol
+	if len(stuff) > 1 {
+		stuff[len(stuff) - 1] = stuff[len(stuff) - 1][:len(stuff[len(stuff) - 1]) - 1]
+	} else {
+		stuff[0] = stuff[0][:len(stuff[0]) - 1]
+	}
+
+	w.WriteString(fmt.Sprintf("let e%d=document.createElement('%s');", elemNumber, stuff[0]))
+
+	textContent, _ := r.ReadString('<')
+	textContent = strings.TrimSpace(textContent)
+
+	if len(textContent) > 1 {
+		w.WriteString(fmt.Sprintf("e%d.textContent='%s';", elemNumber, textContent[:len(textContent) - 1]))
+	}
+
+	for {
+		nextTag, _ := r.Peek(len(stuff[0]) + 1)
+		fmt.Println(elemNumber, stuff[0], string(nextTag))
+		if ; bytes.Compare([]byte("/" + stuff[0]), nextTag) == 0 {
+			r.ReadBytes('>')
+			break
+		}
+
+		num := proccessElement(w, r)
+		w.WriteString(fmt.Sprintf("e%d.appendChild(e%d);", elemNumber, num))
+		_, err := r.ReadBytes('<')
+
+		if err != nil {
+			break;
+		} 	
+	}
+
+	return elemNumber
 }
